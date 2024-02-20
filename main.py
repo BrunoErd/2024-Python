c27548f578fec8e3b6f2eaf33bfeaea4ad0ccab8
Bruno Weber <128698720+BrunoErd@users.noreply.github.com>
2024-02-20 09:30:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ try:
     idade =int (input("Escreva sua Idade!") )
 except ValueError:
     print("vocẽ não digitou um número")
-print(f"Olá, {nome},vocẽ tem {idade} anos")         
+print(f"Olá, {nome},vocẽ tem {idade} anos")    
+
+try:
+    n1 = float(input("digite um número"))
+    n2 = float(input("digite mais um número"))
+except ValueError:
+    print("Tente novamente")
+    
+soma = n1 + n2
+
+print (f"O número {n1} mais o número {n2} é igual a: {soma}.")
+# -*- coding: utf-8 -*-
