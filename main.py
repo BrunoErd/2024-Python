d475f01068cbbe453068673f081cb8a32a9d34e9
Bruno Weber <128698720+BrunoErd@users.noreply.github.com>
2024-02-20 12:02:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-# Solicite a idade a d o usúario 
-nota = int(input("Digite sua nota:"))
-#categorize a idade 
-if  nota > 90:
-    categoria = "Nota A"
-elif nota > 80:
-    categoria = "Nota B"
-elif nota > 70:
-    categoria = "Nota C"
-elif nota > 60:
-    categoria = "Nota D"
-else:
-    categoria = "Nota F"
+# Solicite o més do ano e descubra qual é a estação  
+mes = input("Digite o mes:")
+#categorize o més 
+if mes in ("Dezembro","Janeiro","fevererio"):
+    estação = "Verão"
+elif mes in ("Março","Abril","Maio"):
+    estação = "Outono"
+elif mes in ("Junho","Julho","Agosto"):
+    estação = "Inverno"
+elif mes in  ("setembro","outubro","Dezembro"):
+    estação = "Primavera"
+
     #Exibe a categoria
-print(f"Você está na categoria: {categoria}.")
+print(f"Você está na categoria: {estação}.")
