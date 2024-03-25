4c2de8d07177cca2fd412ebd62ff5294b205a63e
Bruno Weber <128698720+BrunoErd@users.noreply.github.com>
2024-03-24 22:49:49 -0300

    Delete feito_em_sala.py

--- feito_em_sala.py
+++
@@ -1,7 +0,0 @@
-nota1 = float(input("digite a nota1"))
-nota2 = float(input("digite a nota2"))
-nota3 = float(input("digite a nota3"))
-
-media = (nota1 + nota2 + nota3)/3
-
-print (f"a media das notas é: {media}")
